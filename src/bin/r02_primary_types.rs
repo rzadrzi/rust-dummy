71684fc661304ef20317bf91d6e06e526a84a146
rzadrzi <darzi.reza@gmail.com>
2024-06-02 23:59:44 +0330

    primary data type

---
+++ src/bin/r02_primary_types.rs
@@ -0,0 +1,19 @@
+
+/**
+Boolean
+Integer
+    8-bit	i8	u8
+    16-bit	i16	u16
+    32-bit	i32	u32
+    64-bit	i64	u64
+    128-bit	i128	u128
+    arch	isize	usize
+Double/Float
+Character
+String*
+*/
+
+fn main(){
+    println!("Please read comments");
+
+}
