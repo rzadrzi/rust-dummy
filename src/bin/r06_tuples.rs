0848d59ec2ae2a7f2073405dc92ff48f6634ebb1
rzadrzi <darzi.reza@gmail.com>
2024-06-03 08:36:27 +0330

    simple tuple added

---
+++ src/bin/r06_tuples.rs
@@ -0,0 +1,8 @@
+
+
+fn main() {
+    let t = ("reza", 2.65, 3);
+    println!("tuple posion one:\t{:?}", t.0);
+    println!("tuple posion tow:\t{:?}", t.1);
+    println!("tuple posion three:\t{:?}", t.2);
+}
