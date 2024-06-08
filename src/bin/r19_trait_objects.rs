0a9523433d8ce244ced2b5673ea89486020aa6e1
rzadrzi <darzi.reza@gmail.com>
2024-06-08 08:15:30 +0330

    this file has loooooooooooooot of  errorsssssssssssssss

--- src/bin/r19_trait_objects.rs
+++ src/bin/r19_trait_objects.rs
@@ -14,12 +14,40 @@ impl LandCapable for SUV{
 
 trait LandCapable {
     fn drive(&self) {
-        
+        println!("drive on the road")
     }
 }
 
+trait WaterCapable {
+    fn float(&self){
+        println!("dirve on the water")
+    }
+}
+
+// trait Amphibious:LandCapable+WaterCapable {}
+struct Amphibious<T:LandCapable+WaterCapable>{
+    vehicle:T
+}
+
+// impl <T:LandCapable+WaterCapable> Amphibious<T> {
+    
+// }
+// struct Howecraft;
+
+// impl Amphibious for Howecraft {
+//     fn drive(&self){
+//         println!("Howercraft")
+//     }
+    
+// }
+
 fn road_trip(vehicle:&dyn LandCapable){
     vehicle.drive();
+}
+
+fn howercraft<T:LandCapable+WaterCapable>(vehicle:&Amphibious<T>){
+    vehicle.drive();
+    vehicle.float();
 }
 
 fn main(){
