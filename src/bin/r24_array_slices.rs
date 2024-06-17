d2676268e57fe3cf5dec5e13689cb6ce4939786a
rzadrzi <darzi.reza@gmail.com>
2024-06-18 03:13:00 +0330

    unnessery code deleted

--- src/bin/r24_array_slices.rs
+++ src/bin/r24_array_slices.rs
@@ -9,11 +9,7 @@ struct Chars{
 }
 
 trait Object {
-    fn show_items(&self){
-        // for i in self.items{
-        //     println!("{:?}", i)
-        // }
-    }
+    fn show_items(&self){}
 }
 
 impl Object for I32 {
@@ -32,31 +28,17 @@ impl Object for Chars {
     }
     
 }
-// fn show_array(arr:&[i32]){
-//     for i in arr{
-//         println!("{:?}", i)
-//     }
-// }
 
-fn show_slice(slice:&[char]){
-    for i in slice{
-        println!("{:?}", i)
-    }
-}
 
 fn main(){
-    // let arr:[i32; 9] = [1,2,3,4,5,6,7,8,10];
-    // show_array(&arr);
+
     let arr = I32{
         items:[1,2,3,4,5,6,7,8,10]
     };
 
     arr.show_items();
     
-
-    // let charss: [char; 9] = ['A', 'B', 'C', 'D', 'E', 'F', 'I', 'J', 'K'];
-    // let s:&[char]=&charss;
-    // show_slice(s);
+    
     let charss = Chars{
         items:['A', 'B', 'C', 'D', 'E', 'F', 'I', 'J', 'K']
     };
