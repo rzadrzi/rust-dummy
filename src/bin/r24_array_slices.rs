e6b32de605f5d3b464e00d280051abf2fb225d8f
rzadrzi <darzi.reza@gmail.com>
2024-06-18 02:52:37 +0330

    function for showing slice element added

--- src/bin/r24_array_slices.rs
+++ src/bin/r24_array_slices.rs
@@ -4,9 +4,17 @@ fn show_array(arr:&[i32]){
     }
 }
 
+fn show_slice(slice:&[char]){
+    for i in slice{
+        println!("{:?}", i)
+    }
+}
+
 fn main(){
     let arr:[i32; 9] = [1,2,3,4,5,6,7,8,10];
     show_array(&arr);
 
-    let Char: [char; 9] = ['A', 'B', 'C', 'D', 'E', 'F', 'I', 'J', 'K'];
+    let charss: [char; 9] = ['A', 'B', 'C', 'D', 'E', 'F', 'I', 'J', 'K'];
+    let s:&[char]=&charss;
+    show_slice(s);
 }
